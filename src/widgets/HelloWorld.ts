849a2967e0a4f6462351b16792da690bfdcba4ad
Anthony Gubler <agubler@sitepen.com>
2018-10-08 15:47:57 +0100

    ready to test

--- src/widgets/HelloWorld.ts
+++ src/widgets/HelloWorld.ts
@@ -1,5 +1,8 @@
 import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
 import { v } from '@dojo/framework/widget-core/d';
+import uuid from '@dojo/framework/core/uuid';
+import request from '@dojo/framework/core/Request';
+import { Outlet } from '@dojo/framework/routing/Outlet';
 
 import * as css from './styles/helloWorld.m.css';
 
@@ -11,12 +14,17 @@ const logo = require('./../img/logo.svg');
  * Refer to the creating widgets tutorial for help: https://dojo.io/tutorials/003_creating_widgets/
  */
 export class HelloWorld extends WidgetBase {
+	onAttach() {
+		request('https://foo.com');
+	}
 	protected render() {
 		return v('div', { classes: css.root }, [
 			v('img', { src: logo, classes: css.logo }),
-			v('div', { classes: css.label }, ['Hello, Dojo World!'])
+			v('div', { classes: css.label }, ['Hello, Dojo World!', uuid()])
 		]);
 	}
 }
+
+export const a = Outlet(HelloWorld, 'state', { mapParams: () => ({}) });
 
 export default HelloWorld;
